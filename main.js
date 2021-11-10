91d0b3aa10b05818ec7446bc9fef2fc375c512f8
Oksana Ovchinnikova <oksana_ovchinnikova@inbox.ru>
2021-11-10 18:22:45 +0500

    creare repositoriy

---
+++ main.js
@@ -0,0 +1,23 @@
+document.addEventListener('mousemove', (e) => {
+  console.log(e);
+})
+
+const slider = new Swiper('.tariff-cards', {
+  loop: true,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
