d0ea084f6d321721f6dac4a68150212ef08e89e3
Oksana Ovchinnikova <oksana_ovchinnikova@inbox.ru>
2021-11-12 13:31:50 +0500

    create slider

--- main.js
+++ main.js
@@ -1,23 +1,40 @@
-document.addEventListener('mousemove', (e) => {
-  console.log(e);
-})
+// document.addEventListener('mousemove', (e) => {
+//   console.log(e);
+// })
 
-const slider = new Swiper('.tariff-cards', {
-  loop: true,
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
+// const slid = new Swiper('.tariff-cards', {
+//   loop: true,
+//   // If we need pagination
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+// });
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
+const slider = document.querySelector('.tariff-cards');
+let mySwiper;
 
+function mobileSlider() {
+
+  if (window.innerWidth <= 767) {
+    mySwiper = new Swiper('.tariff-cards', {
+      loop: true,
+      // If we need pagination
+      autoplay: {
+        delay: 5000,
+      },
+    });
+    
+
+    slider.dataset.mobile = 'true';
+  }
+
+  if (window.innerWidth > 767) {
+    slider.dataset.mobile = 'false';
+    if (slider.classList.contains('swiper-initialized')) {
+      mySwiper.destroy();
+    }
+  };
+}
+
+
+mobileSlider();
+
+window.addEventListener('resize', mobileSlider);
